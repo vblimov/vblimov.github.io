a79e374df750b1f9ac24a9d999e41b4fcf9753c1
vblimov <vblimov@gmail.com>
2020-11-27 18:15:53 +1000

    Initial commit

---
+++ js/loginForm.js
@@ -0,0 +1,145 @@
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-password');
+const loginSignUp = document.querySelector('.login-signUp');
+const loginForget = document.querySelector('.login-forget');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editElemContainer = document.querySelector('.edit-container');
+const editButton = document.querySelector('.edit-button');
+
+const editUsername = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+const userAvatarElem = document.querySelector('.user-avatar');
+
+
+
+const setUsers = {
+  user: null,
+  initUser(handler){
+    auth.onAuthStateChanged(user => {
+      if(user){
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+      if(handler) {
+        handler();
+      }
+    })
+  },
+  logIn(email, password, handler) {
+    auth.signInWithEmailAndPassword(email, password)
+        .catch((err) => {
+          alert(err.message)
+        })
+
+  },
+  logOut() {
+    auth.signOut();
+    clearFields();
+    toggleAuthDom();
+  },
+  signUp(email, password, handler) {
+    
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((data) => {
+          this.editUser(email.substring(0, email.indexOf('@')), null, handler)})
+        .catch((err) => {
+          alert(err.message)
+        });
+  },
+  editUser(displayName, photoURL, handler){
+    
+    const user = auth.currentUser;
+    
+    if(displayName){
+      if(photoURL){
+        user.updateProfile({
+          displayName,
+          photoURL
+        }).then(handler)
+      } else {
+        user.updateProfile({
+          displayName
+        }).then(handler)
+      }
+    }
+
+  },
+  sendForget(email){
+    if(email){
+    auth.sendPasswordResetEmail(email)
+        .then(() => {
+          alert('Письмо отправлено')})
+    } else
+    {
+      alert('Введите email')
+    }
+  }
+};
+
+const emailValidator = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const clearFields = () => {
+  loginForm.reset();
+}
+
+const loginFormInit = () => {
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
+  });
+
+  loginSignUp.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = emailInput.value;
+    if(emailValidator(email)){
+      setUsers.signUp(email, passwordInput.value, toggleAuthDom);
+    } else {
+      alert('Неправильно введён email');
+      clearFields();
+    }
+  });
+
+  exitElem.addEventListener('click', (event) =>{
+    event.preventDefault();
+    setUsers.logOut();
+  });
+
+  editElem.addEventListener('click', event =>{
+    event.preventDefault();
+    editElemContainer.classList.toggle('visible');
+    editUsername.value = setUsers.user.displayName;
+  });
+  editElemContainer.addEventListener('submit', event =>{
+
+    event.preventDefault();
+    setUsers.editUser(editUsername.value, editPhotoURL.value, toggleAuthDom);
+    editElemContainer.classList.remove('visible');
+  });
+  
+  loginForget.addEventListener('click', event => {
+    event.preventDefault();
+    setUsers.editUser(emailInput.value);
+    emailInput.value
+  })
+  
+  setUsers.initUser(toggleAuthDom);
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  loginFormInit();
+})
+
+
+
+
---
+++ js/postsHandler.js
@@ -0,0 +1,194 @@
+const postsWrapper = document.querySelector('.posts');
+const newPostButton = document.querySelector('.button-new-post');
+const addPostForm = document.querySelector('.add-post');
+const editPostForm = document.querySelector('.edit-post');
+let editablePostID;
+const showAllPosts = () => {
+    let postsHTML = '';
+    let posts = [];
+    database.ref('post').orderByChild('date').once('value', snapshot => {
+        (snapshot || []).forEach(item=>{
+            posts.push(`
+              <section class="post">
+                <div class="post-body">
+                  <h2 class="post-title">${item.val().title}</h2>
+                  <p class="post-text" >${item.val().text}</p>
+                  <div class="tags">
+                    ${(item.val().tags || []).map(tag=>`<a href="#" class="tag">#${tag}</a>`)}
+                  </div>
+                </div>
+                <div class="post-footer">
+                  <div class="post-buttons">
+                    <button class="post-button post-button-likes">
+                <svg width="19" height="20" class="icon icon-like">
+                  <use xlink:href="img/icons.svg#like"></use>
+                </svg>
+                <span class="post-button likes-counter">${item.val().like}</span>
+              </button>
+                    <button class="post-button post-button-comments">
+                <svg width="21" height="21" class="icon icon-comment">
+                  <use xlink:href="img/icons.svg#comment"></use>
+                </svg>
+              <span class="comments-counter">${item.val().comments}</span>
+            </button>
+                    <button class="post-button post-button-save">
+                <svg width="19" height="19" class="icon icon-save">
+                  <use xlink:href="img/icons.svg#save"></use>
+                </svg>
+            </button>
+                    <button class="post-button post-button-share">
+                <svg width="17" height="19" class="icon icon-share">
+                  <use xlink:href="img/icons.svg#share"></use>
+                </svg>
+            </button>
+                    ${setUsers.user? item.val().author.email === (setUsers.user.email || null) || setUsers.user.email === 'vblimov@gmail.com'?
+                          `<button class="post-button post-button-delete" value="${item.val().id}" onclick="deletePost(value)">
+                  <svg width="17" height="19" class="icon icon-delete">
+                    <use xlink:href="img/icons.svg#delete"></use>
+                  </svg>
+                </button>`: `` :``}
+                    ${setUsers.user? item.val().author.email === (setUsers.user.email || null) || setUsers.user.email === 'vblimov@gmail.com'?
+                `<button class="post-button post-button-edit" value="${item.val().id}" onclick="editPost(value)">
+                  <svg width="17" height="19" class="icon icon-edit">
+                    <use xlink:href="img/icons.svg#edit"></use>
+                  </svg>
+                </button>`: `` :``}
+                  </div>
+                  <div class="post-author">
+                    <div class="author-about">
+                      <a href="#" class="author-username">${item.val().author.displayName}</a>
+                      <span class="post-time">${item.val().date}</span>
+                    </div>
+                  <a href="#" class="author-link"><img src=${item.val().author.photo || "img/avatar.jpeg"} alt="avatar" class="author-avatar"></a>
+                </div>
+                </div>
+              </section>`);
+        })
+    }).then(()=> {
+        posts.reverse().forEach(post => {postsHTML+=post;})
+        postsWrapper.innerHTML = postsHTML;
+        postsHTML = '';
+        addPostForm.classList.remove('visible');
+        editPostForm.classList.remove('visible');
+        postsWrapper.classList.add('visible');
+    })
+}
+const showAddPosts = () => {
+    addPostForm.classList.add('visible');
+    postsWrapper.classList.remove('visible');
+    editPostForm.classList.remove('visible');
+    editPostForm.reset();
+}
+const showEditPosts = (title, text, tags, postID) => {
+    editPostForm.classList.add('visible');
+    editPostForm.elements.namedItem('edit-post-title').value = title;
+    editPostForm.elements.namedItem('edit-post-text').value = text;
+    editPostForm.elements.namedItem('edit-post-tags').value = tags;
+    editablePostID = postID;
+    postsWrapper.classList.remove('visible');
+}
+const postHandlerInit = () => {
+    showAllPosts();
+    
+    newPostButton.addEventListener('click', event => {
+        event.preventDefault();
+        showAddPosts();
+    })
+    
+    addPostForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const formElements = addPostForm.elements;
+        //TODO вернуть нормальные значениея
+        if(formElements.namedItem('post-title').value.length < 0){
+            alert('Слишком короткий заголовок');
+            return;
+        }
+        if(formElements.namedItem('post-text').value.length < 0){
+            alert('Слишком короткий пост');
+            return;
+        }
+        const postID = 'postID'+(+new Date()).toString(16)
+        database.ref('post/'+postID).set({
+            id: postID,
+            title: formElements.namedItem('post-title').value,
+            text: formElements.namedItem('post-text').value,
+            tags: formElements.namedItem('post-tags').value.split(',').map(item=>item.trim()),
+            author: {
+                displayName: setUsers.user.displayName,
+                photo: setUsers.user.photoURL,
+                email: setUsers.user.email
+            },
+            date: new Date().toLocaleString(),
+            like: 0,
+            comments: 0,
+        })
+        showAllPosts();
+        
+        addPostForm.reset();
+    })
+    
+    editPostForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const formElements = editPostForm.elements;
+        //TODO вернуть нормальные значениея
+        if(formElements.namedItem('edit-post-title').value.length < 0){
+            alert('Слишком короткий заголовок');
+            return;
+        }
+        if(formElements.namedItem('edit-post-text').value.length < 0){
+            alert('Слишком короткий пост');
+            return;
+        }
+        
+        database.ref('post/'+editablePostID).update({
+            id: editablePostID,
+            title: formElements.namedItem('edit-post-title').value,
+            text: formElements.namedItem('edit-post-text').value,
+            tags: formElements.namedItem('edit-post-tags').value.split(',').map(item=>item.trim()),
+            author: {
+                displayName: setUsers.user.displayName,
+                photo: setUsers.user.photoURL,
+                email: setUsers.user.email
+            },
+            date: new Date().toLocaleString(),
+            like: 0,
+            comments: 0,
+        })
+        showAllPosts();
+        
+        editPostForm.reset();
+    })
+}
+
+const deletePost = (postID) => {
+    database.ref('post')
+        .once('value', snap => {
+            snap.forEach(item => {
+                if(item.val().id === postID){
+                    database.ref('post').child(item.key).remove()
+                        .then(()=>{
+                            showAllPosts()
+                            return true;
+                        })
+                }
+            })
+        })
+    
+}
+
+const editPost = (postID) => {
+    database.ref('post')
+        .once('value', snap => {
+            snap.forEach(item => {
+                if(item.val().id === postID){
+                    showEditPosts(item.val().title, item.val().text, item.val().tags, postID)
+                }
+            })
+        })
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    postHandlerInit();
+})
+
+
